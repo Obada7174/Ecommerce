8f9aa24cdfec06c960c72f8850f9948567924162
Obada7174 <144602262+Obada7174@users.noreply.github.com>
2025-01-21 10:15:51 -0800

    Update baseUrl

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,4 +1,4 @@
-export const baseURL = "http://127.0.0.1:8000/api";
+export const baseURL = "https://ecommerce-backend.obada-almaghribi.com/api";
 export const register = "register";
 export const login = "login";
 export const logout = "logout";
